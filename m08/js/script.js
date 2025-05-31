e0c751e99a7fbe97647279afc8985f1c43981524
Joel Samuel <joel.samuel@bellevuecollege.edu>
2025-05-31 16:51:51 -0700

    update js file

--- m08/js/script.js
+++ m08/js/script.js
@@ -1,1 +1,15 @@
+function Hotel(name, rooms, booked) {
+  this.name = name;
+  this.rooms = rooms;
+  this.booked = booked;
+  this.checkAvailability = function () {
+    return this.rooms - this.booked;
+  };
+}
 
+var hotel1 = new Hotel('Quay', 40, 25);
+var hotel2 = new Hotel('Park', 120, 77);
+var hotel3 = new Hotel('Sunset', 86, 10);
+
+var el = document.getElementById('hotel3');
+el.textContent = hotel3.name + ' rooms available: ' + hotel3.checkAvailability();
