90d9843203ff857b477c5d8b5ddeb3b0189db853
Joel Samuel <joel.samuel@bellevuecollege.edu>
2025-05-31 16:57:31 -0700

    add comments

--- m08/js/script.js
+++ m08/js/script.js
@@ -1,3 +1,4 @@
+// Hotel constructor function
 function Hotel(name, rooms, booked) {
   this.name = name;
   this.rooms = rooms;
@@ -7,9 +8,11 @@ function Hotel(name, rooms, booked) {
   };
 }
 
+// Create three hotel objects
 var hotel1 = new Hotel('Quay', 40, 25);
 var hotel2 = new Hotel('Park', 120, 77);
 var hotel3 = new Hotel('Sunset', 86, 10);
 
+// Display third hotel using JavaScript
 var el = document.getElementById('hotel3');
 el.textContent = hotel3.name + ' rooms available: ' + hotel3.checkAvailability();
