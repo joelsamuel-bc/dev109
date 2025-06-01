1c30aecd0487d006f6b95679359f5ea4c57f637d
Joel Samuel <joel.samuel@bellevuecollege.edu>
2025-05-31 17:17:52 -0700

    Update script.js

--- m08/js/script.js
+++ m08/js/script.js
@@ -1,18 +1,24 @@
-// Hotel constructor function
-function Hotel(name, rooms, booked) {
+// Constructor function for Hotel objects
+function Hotel(name, totalRooms, bookedRooms) {
   this.name = name;
-  this.rooms = rooms;
-  this.booked = booked;
+  this.totalRooms = totalRooms;
+  this.bookedRooms = bookedRooms;
   this.checkAvailability = function () {
-    return this.rooms - this.booked;
+    return this.totalRooms - this.bookedRooms;
   };
 }
 
-// Create three hotel objects
-var hotel1 = new Hotel('Quay', 40, 25);
-var hotel2 = new Hotel('Park', 120, 77);
-var hotel3 = new Hotel('Sunset', 86, 10);
+// Create hotel objects
+const quayHotel = new Hotel("Quay", 50, 35);
+const parkHotel = new Hotel("Park", 120, 77);
+const sunsetHotel = new Hotel("Sunset", 86, 10); // NEW object
 
-// Display third hotel using JavaScript
-var el = document.getElementById('hotel3');
-el.textContent = hotel3.name + ' rooms available: ' + hotel3.checkAvailability();
+// Update the page with availability
+document.getElementById("hotel1").textContent =
+  quayHotel.name + " rooms: " + quayHotel.checkAvailability();
+
+document.getElementById("hotel2").textContent =
+  parkHotel.name + " rooms: " + parkHotel.checkAvailability();
+
+document.getElementById("hotel3").textContent =
+  sunsetHotel.name + " rooms: " + sunsetHotel.checkAvailability();
